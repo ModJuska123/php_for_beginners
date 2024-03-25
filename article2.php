5209f2585cb403f20b64f3b414cd1a0777402a6d
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-03-25 17:17:25 +0200

    Update

--- article2.php
+++ article2.php
@@ -1,12 +1,11 @@
 <?php
 
 $db_host = "localhost";
-$db_name = "cms_new";
-$db_user = "cmsnew_www";
-$db_pass = "TPoPhUXeTuHqtTwM";
+$db_name = "lecture";
+$db_user = "root";
+$db_pass = "";
 
-$conn = mysqli_connect($db_host, $db_name, $db_user, $db_pass);
-
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (mysqli_connect_error()) {
     echo mysqli_connect_error();
@@ -15,7 +14,7 @@ if (mysqli_connect_error()) {
 
 $sql = "SELECT *
         FROM article
-        WHERE id = 1";
+        WHERE id = 2";
 
 $results = mysqli_query($conn, $sql);
 
