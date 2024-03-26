aae9c1658eb6204aebd779ec1b7a55af2befd79a
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-03-26 18:25:39 +0200

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_name = "cms";
 $db_user = "cms_www";
-$db_pass = "_W@RXE)5dHtGSDJ_";
+$db_pass = "";
 
 $conn = mysqli_connect($db_host, $db_name, $db_user, $db_pass);
 
---
+++ siurblioTestavimas.php
@@ -0,0 +1,26 @@
+<h1>Dulkių siurblys/robotas: ką būtina testuoti (testavimo rūšys/testavimo veiksmai):</h1>
+
+    <h2>a. Funkcinis:</h2>
+        <h4>1. ar įsijungia paspaudus įjungimo mygtuką, tyrėtų apsišviesti ekranas.</h4>
+        <h4>2. ar paleidus automatinį rėžimą savarankiškai atlieka patalpos siurbimą, turėtų išsiurbti grindis ir grįžti į pasikrovimo vietą.</h4>
+        <h4>3. ar susiurbia šiukšles siurbiamoje patalpoje, turėtų surinkti daugiau, kaip 90 proc. šiukšlių vienu siurbimu.</h4>
+        <h4>4. ar nustačius atsikartojančius laikus atlieka numatytus siurbimus, turėtų nustatytu laiku pradėti siurbimo darbą.</h4>
+
+    <h2>b. Apkrovimas (dideliam, vidutiniam, mažam šiukšlių kiekiui):</h2>
+        <h4>1. ar akumuliatoriaus talpa pakankama siurbimo darbui atlikti, vienu užkrovimu turi išsiurbti 100, 120, 140 m2 ploto patalpą.</h4>
+        <h4>2. po kiek siurbimo darbo valandų pilnai užsiteršia, turi be valymo išsiurbti 100, 120, 140 m2 plotą.</h4>
+
+    <h2>c. Suderinamumas:</h2>
+        <h4>1. ar el. kištukas tinkamas europiniams el. lizdams, turėtų atitikti ES standartą.</h4>
+
+    <h2>d. Saugumas:</h2>
+        <h4>1. ar įrangai reikalingas programinės įrangos atnaujinimas, turėtų savarankiškai atsinaukinti, prisijungus prie wifi.</h4>
+        <h4>2. ar siurblys geba identifikuoti vietas su laiptais, turėtų laiptus ir kitas nesaugias vietas identifikuoti ir nevažiuoti ant jų.</h4>
+        <h4>3. ar saugus naminiams gyvūnams, vaikams, suaugusiems, turėtų išsijungti pakėlus nuo žemės, neužvažiuoti ant kojų.</h4>
+
+    <h2>e. Atsigavimas:</h2>
+        <h4>1. ar siurblys greitai pasikrauna esant pilnam jo iškrovimui, pilnai pasikrauti turi ne ilgiau, kaip per 1 val.</h4>
+
+    <h2>f. Panaudojamumas:</h2>
+        <h4>1. ar patogu siurblį naudotis vartotojui, užstrigimų skaičius neturi viršyti 1 kartą per 100 darbo valandų standartinėje patalpoje.</h4>
+        <h4>2. ar yra instrukcijos LT kalba, instrukcijos turi būti išverstos į LT kalbą, turėti vaisdinį ir rašytinį apibudinimą.</h4>
